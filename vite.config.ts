839f1e5e14dbdd89d7200dd87670c12a495a4847
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-07-31 14:51:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/zappy/", // 👈 Add this line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
